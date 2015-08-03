b81c69e53786e8838692f216081e7e2bc9d952d8
skral <sebastian.kral@pixomondo.com>
2015-08-03 11:38:04 -0400

    - added default_node_name feature. - added convenience function to bring in alembic node from output path (works only in /obj/... network). - refactored to store code in internal PythonModule in OTL instead of under the functions itself. - added 'Jump to File System' and 'Copy to Clipboard' buttons. - refactored code to have almost everything in the handler instead of the OTL. - include node info when a warning is printed. - include functionality to convert sgtk alembic to alembic nodes. - include tile color when converting from sgtk to alembic/geometry nodes - use SGTK tab instead of cluttering the UI with features that aren't used to often.

--- app.py
+++ app.py
@@ -19,3 +19,18 @@ class AlembicOutputNode(sgtk.platform.Application):
     def init_app(self):
         module = self.import_module("tk_houdini_alembicnode")
         self.handler = module.ToolkitAlembicNodeHandler(self)
+
+    def convert_to_alembic_nodes(self):
+        """
+        Convert all Shotgun Alembic nodes found in the current Script to regular
+        Alembic nodes. Additional toolkit information will be stored in
+        user data named 'tk_*'
+        """
+        self.handler.convert_sg_to_alembic_nodes()
+
+    def convert_from_alembic_nodes(self):
+        """
+        Convert all regular Alembic nodes that have previously been converted
+        from Shotgun Alembic nodes, back into Shotgun Alembic nodes.
+        """
+        self.handler.convert_alembic_to_sg_nodes()
