c8b6e7e6ba2e2e7208b0e5d6b797aa3e49a2a3b5
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2015-10-22 16:07:19 -0400

    SG license back in from Pixomondo
    * zendesk ticket #43514 for reference

--- python/tk_houdini_alembicnode/handler.py
+++ python/tk_houdini_alembicnode/handler.py
@@ -1,12 +1,12 @@
-# Copyright (c) 2015 Pixomondo
-#
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
 # CONFIDENTIAL AND PROPRIETARY
-#
-# This work is provided "AS IS" and subject to the MIT License included in this
-# distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your
-# agreement to the MIT License. All rights
-# not expressly granted therein are reserved by Pixomondo.
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
 
 import os
 import sys
